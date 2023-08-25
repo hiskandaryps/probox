ca247cee3021c734b4382980aa6c8de529e459e5
hiskandar <hiskandaryapis@gmail.com>
2023-08-25 12:40:14 +0700

    jwt middleware created

--- index.js
+++ index.js
@@ -22,6 +22,21 @@ function formatTimestamp(timestamp) {
   return format(zonedTime, 'yyyy-MM-dd HH:mm:ss', { timeZone });
 }
 
+function authenticateToken(req, res, next) {
+  const token = req.header('Authorization');
+
+  if (!token) {
+    return res.status(401).json({ success: false, message: 'Access denied. No token provided.' });
+  }
+
+  jwt.verify(token, JWT_SECRET, (err) => {
+    if (err) {
+      return res.status(403).json({ success: false, message: 'Invalid token.' });
+    }
+    next();
+  });
+}
+
 app.post('/api/login', async (req, res) => {
   const { userEmail } = req.body;
 
