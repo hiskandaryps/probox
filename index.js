d8914856e94176e2f68487afe39cbc1104b25760
hiskandar <hiskandaryapis@gmail.com>
2023-08-25 11:22:49 +0700

    /api/login updated

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { createClient } = require('@supabase/supabase-js');
 const { format } = require("date-fns");
 const { utcToZonedTime } = require('date-fns-tz');
 const cors = require('cors');
+const JWT_SECRET = process.env.JWT_SECRET || 'hohoh';
 require("dotenv").config();
 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
@@ -19,6 +20,35 @@ function formatTimestamp(timestamp) {
   const zonedTime = utcToZonedTime(timestamp, timeZone);
   return format(zonedTime, 'yyyy-MM-dd HH:mm:ss', { timeZone });
 }
+
+app.post('/api/login', async (req, res) => {
+  const { userEmail } = req.body;
+
+  try {
+    // Query the Supabase database to check if the userEmail exists in the users table
+    const { data, error } = await supabase
+      .from('users')
+      .select('email')
+      .eq('email', userEmail)
+      .single();
+
+    if (error) {
+      throw error;
+    }
+
+    if (!data) {
+      return res.status(401).json({ success: false, message: 'Authentication failed. User not found.' });
+    }
+
+    // User with the provided email exists, generate a JWT token with userEmail
+    const token = jwt.sign({ userEmail }, JWT_SECRET, { expiresIn: '1h' });
+
+    // Send the token in the response
+    res.json({ success: true, message: 'Authentication successful.', token });
+  } catch (error) {
+    res.status(500).json({ success: false, error: 'Error during authentication: ' + error.message });
+  }
+});
 
 app.get('/api/probox', async (req, res) => {
   try {
